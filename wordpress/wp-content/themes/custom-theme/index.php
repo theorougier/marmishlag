65c01a29d25bdc6aaba2e70ad353bd1699e51426
TheoRougier-enoki <67681656+TheoRougier-enoki@users.noreply.github.com>
2022-04-04 11:25:42 +0200

    push

--- wordpress/wp-content/themes/custom-theme/index.php
+++ wordpress/wp-content/themes/custom-theme/index.php
@@ -1,2 +1,7 @@
 <?php get_header(); ?>
+    <?php $loop = new WP_Query( array( 'post_type' => 'Recettes', 'posts_per_page' => '10' ) ); ?>
+    <?php while ( $loop->have_posts() ) : $loop->the_post(); ?>
+           <h1><?php the_title() ?></h1>
+           <?php the_content() ?>
+    <?php endwhile; wp_reset_query(); ?>
 <?php get_footer(); ?>
